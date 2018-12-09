8a622bf57a7ea38129a488dfbbe5c24eefe2be84
Alexey Shumakov <bobshaftoe@yandex.ru>
2018-12-09 22:14:05 +0200

    init

---
+++ backend/config/config.exs
@@ -0,0 +1,30 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+config :backend,
+  ecto_repos: [Backend.Repo]
+
+# Configures the endpoint
+config :backend, BackendWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "13duJKw3Nx6FB2yrqAYEk4svF/hh6Zz6w1bWSpGxD/tDSeBGeeFAIKDNEF3Ynger",
+  render_errors: [view: BackendWeb.ErrorView, accepts: ~w(json)],
+  pubsub: [name: Backend.PubSub, adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
