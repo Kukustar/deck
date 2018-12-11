04a9871201513b9d117a94d3fb19f191051807ef
Alexey Shumakov <bobshaftoe@yandex.ru>
2018-12-11 21:36:58 +0200

    simple event

--- client/src/App.js
+++ client/src/App.js
@@ -2,24 +2,25 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {Socket} from "phoenix";
+let socket = new Socket("ws://localhost:4000/socket")
+
+socket.connect()
+
+let channel = socket.channel("deck:main", {})
+
+channel.join()
+  .receive("ok", resp => { console.log("Joined successfully", resp) })
+  .receive("error", resp => { console.log("Unable to join", resp) })
+
+function send() {
+  channel.push("new_msg", {body: "hello"})
+}
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <button onClick={send}>click</button>
       </div>
     );
   }
