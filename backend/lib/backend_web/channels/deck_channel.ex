f8d020061906d9aba831a90b894bbd664dd5f5be
Alexey Shumakov <bobshaftoe@yandex.ru>
2018-12-11 20:32:26 +0200

    create deck channel

---
+++ backend/lib/backend_web/channels/deck_channel.ex
@@ -0,0 +1,12 @@
+defmodule BackendWeb.DeckChannel do
+  use Phoenix.Channel
+
+  def join("deck:main", _message, socket) do
+    {:ok, socket}
+  end
+
+  def handle_in("new_msg", %{"body" => body}, socket) do
+    broadcast!(socket, "new_msg", %{body: body})
+    {:noreply, socket}
+  end
+end
