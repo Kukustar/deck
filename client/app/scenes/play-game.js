c4c684a749a60d2eed0707621570694a865478e4
Kukustar <aa.vv.bogachev@gmail.com>
2018-12-15 11:40:22 +0200

    starting render coming state

--- client/app/scenes/play-game.js
+++ client/app/scenes/play-game.js
@@ -10,7 +10,7 @@ export default class playGame extends Phaser.Scene {
     playGame.socket = new Socket('ws://localhost:4000/socket');
     playGame.socket.connect();
     playGame.channel = playGame.socket.channel('deck:main', {});
-    playGame.channel.on('new_msg', (event) => console.info(event));
+    playGame.channel.on('new_msg', (event) => playGame.renderNewGameBoardState(event));
     playGame.channel.join();
 
     playGame.boardArray = [];
@@ -19,18 +19,14 @@ export default class playGame extends Phaser.Scene {
       for (let j = 0; j < gameOptions.boardSize.cols; j++) {
         const position = playGame.getTitlePosition(i, j);
         const image = this.add.image(position.x, position.y, 'emptytyle')
+
             .setInteractive();
-
-
         image.on('pointerdown', () => playGame.addNewSprite(i, j));
 
-
-        const tile = this.add.sprite(position.x, position.y, 'tiles', 1)
+        const tile = this.add.sprite(position.x, position.y, 'tiles', 2)
             .setInteractive();
 
         tile.visible = false;
-
-        // tile.on('pointerdown', () => playGame.test(i, j));
 
         playGame.boardArray[i][j] = {
           tileValue: 0,
@@ -39,9 +35,7 @@ export default class playGame extends Phaser.Scene {
       }
     }
   }
-  static sayHello(evt) {
-    console.info(evt);
-  }
+
   static getTitlePosition(row, col) {
     const posX = gameOptions.titleSpacing * (col + 1) + gameOptions.titleSize *
             (col + 0.5);
@@ -50,23 +44,42 @@ export default class playGame extends Phaser.Scene {
 
     return new Phaser.Geom.Point(posX, posY);
   }
-  static addNewSprite(i, j) {
-    playGame.boardArray[i][j].tileValue = 3;
+  static renderNewGameBoardState(event){
+    console.info(event.body);
+    const boardState = JSON.parse(event.body);
+    console.info(typeof(boardState));
+
+    const orderedBoardState = {};
+    Object.keys(boardState).sort().forEach(key => orderedBoardState[key] = boardState[key]);
+
+    for (let [key, value] of Object.entries(orderedBoardState)){
+      console.info(key, value);
+    }
+  }
+
+  static renderNewObject(i, j) {
+    playGame.boardArray[i][j].tileValue = 2;
     playGame.boardArray[i][j].tileSprite.visible = true;
-    playGame.boardArray[i][j].tileSprite.setFrame(0);
+    playGame.boardArray[i][j].tileSprite.setFrame(2);
+  }
+
+  static addNewSprite(i, j) {
+    playGame.renderNewObject(i,j);
+
     const board = {};
+    let counter = 0;
+
     for (let i = 0; i < gameOptions.boardSize.rows; i++) {
       for (let j = 0; j < gameOptions.boardSize.cols; j++) {
-        const tmpKey = 'a' + (i+j);
+        counter += 1;
+        const tmpKey = 'a' + counter;
+        console.info(tmpKey);
         board[tmpKey] = playGame.boardArray[i][j].tileValue;
       }
     }
-    // playGame.webSockets.send(JSON.stringify(playGame.boardArray));
 
-    // playGame.webSockets.send(JSON.stringify(board));
-    console.info('before push');
     const msg = {body: JSON.stringify(board)};
-    console.info(msg);
+
     playGame.channel.push('new_msg', msg);
   }
 }
