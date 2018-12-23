689ee7a892d253af925ea5046b43a8317574b0b2
Kukustar <aa.vv.bogachev@gmail.com>
2018-12-23 14:31:33 +0200

    add setting tools

--- client/app/scenes/play-game.js
+++ client/app/scenes/play-game.js
@@ -8,21 +8,34 @@ export default class playGame extends Phaser.Scene {
     }
 
     create() {
-        playGame.socket = new Socket('ws://localhost:4000/socket');
-        playGame.socket.connect();
-        playGame.channel = playGame.socket.channel('deck:main', {});
-        playGame.channel.on('new_msg', (event) => playGame.renderNewGameBoardState(event));
-        playGame.channel.join();
+        playGame.connectToWebSockets();
 
-        playGame.boardArray = [];
-        for (let i = 0; i < gameOptions.boardSize.rows; i++) {
+        playGame.preparePlayGameBoard();
+
+        playGame.renderGameBoard();
+
+        playGame.renderSettingGameBoard();
+    }
+
+    static renderSettingGameBoard() {
+        [1, 2, 3].forEach(frameNumber => {
+            const position = playGame.getTitlePosition(0, frameNumber - 1);
+            this.add.image(position.x, position.y, 'emptytyle');
+            const tile = this.add.sprite(position.x, position.y, 'tiles', frameNumber).setInteractive();
+            tile.on('pointerdown', () => playGame.prepareToAddNewSprite(frameNumber));
+
+        });
+    }
+
+    static renderGameBoard() {
+        for (let i = 1; i < gameOptions.boardSize.rows; i++) {
             playGame.boardArray[i] = [];
             for (let j = 0; j < gameOptions.boardSize.cols; j++) {
                 const position = playGame.getTitlePosition(i, j);
                 const image = this.add.image(position.x, position.y, 'emptytyle')
                     .setInteractive();
 
-                image.on('pointerdown', () => playGame.addNewSprite(i, j, 2));
+                image.on('pointerdown', () => playGame.addNewSprite(i, j, playGame.addSpriteState));
 
                 const tile = this.add.sprite(position.x, position.y, 'tiles', 2)
                     .setInteractive();
@@ -35,6 +48,24 @@ export default class playGame extends Phaser.Scene {
                 };
             }
         }
+    }
+
+    static connectToWebSockets() {
+        playGame.socket = new Socket('ws://localhost:4000/socket');
+        playGame.socket.connect();
+        playGame.channel = playGame.socket.channel('deck:main', {});
+        playGame.channel.on('new_msg', (event) => playGame.renderNewGameBoardState(event));
+        playGame.channel.join();
+    }
+
+    static preparePlayGameBoard() {
+        playGame.boardArray = [];
+        playGame.settingsArray = [];
+        playGame.addSpriteState = null;
+    }
+
+    static prepareToAddNewSprite(frame){
+        playGame.addSpriteState = frame;
     }
 
     static getTitlePosition(row, col) {
@@ -57,13 +88,16 @@ export default class playGame extends Phaser.Scene {
     }
 
     static renderNewGameBoardState(event) {
+        console.info(event);
         const boardState = playGame.parseKeBoardState(JSON.parse(event.body));
         let counter = 0;
 
-        for (let i = 0; i < gameOptions.boardSize.rows; i++) {
+        for (let i = 1; i < gameOptions.boardSize.rows; i++) {
             for (let j = 0; j < gameOptions.boardSize.cols; j++) {
-                playGame.renderNewObject(i,j, boardState[counter]);
-                console.info(i,j, boardState[counter]);
+                if(boardState[counter] > 0){
+                    playGame.renderNewObject(i,j, boardState[counter]);
+                    console.info(i,j, boardState[counter]);
+                }
                 counter+=1;
             }
 
@@ -73,28 +107,31 @@ export default class playGame extends Phaser.Scene {
     static renderNewObject(i, j, frame) {
         playGame.boardArray[i][j].tileValue = frame;
         console.info('renderNewObject', frame);
-        if(frame > 0){
-            playGame.boardArray[i][j].tileSprite.visible = true;
-        }
+        playGame.boardArray[i][j].tileSprite.visible = true;
         playGame.boardArray[i][j].tileSprite.setFrame(frame);
     }
 
     static addNewSprite(i, j, frame) {
-        playGame.renderNewObject(i, j, frame);
+        if(frame !== null){
+            playGame.renderNewObject(i, j, frame);
 
-        const board = {};
-        let counter = 0;
+            const board = {};
+            let counter = 0;
 
-        for (let i = 0; i < gameOptions.boardSize.rows; i++) {
-            for (let j = 0; j < gameOptions.boardSize.cols; j++) {
+            for (let i = 1; i < gameOptions.boardSize.rows; i++) {
+                for (let j = 0; j < gameOptions.boardSize.cols; j++) {
 
-                const tmpKey = 'a' + counter;
-                board[tmpKey] = playGame.boardArray[i][j].tileValue;
-                counter += 1;
+                    const tmpKey = 'a' + counter;
+                    board[tmpKey] = playGame.boardArray[i][j].tileValue;
+                    counter += 1;
+                }
             }
-        }
-        const msg = {body: JSON.stringify(board)};
+            const msg = {body: JSON.stringify(board)};
 
-        playGame.channel.push('new_msg', msg);
+            playGame.channel.push('new_msg', msg);
+
+        }
+        playGame.addSpriteState = null;
+
     }
 }
